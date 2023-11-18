b5efdc27e5225820e0ddc154799c2d53feb5850d
Оксана <126274941+OzornayBelka@users.noreply.github.com>
2023-11-18 19:20:33 +0300

    Revert "Feature/query"

--- src/main/java/Handler.java
+++
@@ -1,7 +0,0 @@
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-
-@FunctionalInterface
-public interface Handler {
-    public void handler (Request request, BufferedOutputStream responseStream) throws IOException;
-}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,10 @@
-import java.io.BufferedOutputStream;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
 
 public class Main {
-
-
     public static void main(String[] args) throws IOException {
-        var server = new Server(9999, 64);
+       Server server = new Server(9999, 64);
 
-        // добавление хендлеров (обработчиков)
-        server.addHandler("GET", "/messages",
-                (request, responseStream) -> {
-                    server.resourceNotFound(responseStream);
-                });
-
-
-        server.addHandler("POST", "/messages",
-                (request, responseStream) -> {
-                    server.resourceNotFound(responseStream);
-                });
-
-        server.start();
-
-
+       server.startServer();
     }
 }
 
